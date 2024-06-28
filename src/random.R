e1482e8da2edc3e78c76500976e182cb608a5777
Aaron Kusmec <amkusmec@github.com>
2024-06-28 13:22:48 -0500

    Removing unused files

--- src/random.R
+++
@@ -1,6 +0,0 @@
-### Variable definitions:
-###  - taxa = character vector of sample IDs; used as row/col names for matrices
-###  - n = integer size of desired training set
-
-# Select a random subset of size n from taxa
-random <- function(taxa, n) sample(taxa, n, replace = FALSE)
